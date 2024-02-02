32082667257e8b01ad4b9eec5d04d70c412106c2
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-02-02 15:50:55 -0300

    update

--- cnsa-web/src/App.jsx
+++ cnsa-web/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
           </header>
           <main>
             <Banner/>
-            <Steps/>
             <Parallax/>
+            <Steps/>
             <Diferenciais/>
 
             <WhatsAppButton/>
--- cnsa-web/src/assets/components/Banner.jsx
+++ cnsa-web/src/assets/components/Banner.jsx
@@ -1,17 +1,28 @@
-
 import styles from '../css/Banner.module.css';
 
 import banner1 from '../images/cnsa-banner-default.png';
+import banner2 from '../images/cnsa-banner-default-2.png';
+import banner3 from '../images/cnsa-banner-default-3.png';
+import banner4 from '../images/cnsa-banner-default-4.png';
 
 const Banner = () => {
 
     return (
-        <>
-            <section className={styles.containerBanner}>
-                <img src={banner1} alt="banner logo" />
-            </section>
-        </>
-    )
-}
+        <section className={styles.sliderContainer}>
+            <div>
+                <img src={banner1} alt="banner logo"/>
+            </div>
+            <div>
+                <img src={banner2} alt="banner logo 2"/>
+            </div>
+            <div>
+                <img src={banner3} alt="banner logo 3"/>
+            </div>
+            <div>
+                <img src={banner4} alt="banner logo 4"/>
+            </div>
+        </section>
+    );
+};
 
-export default Banner; 
+export default Banner;
