7bc2754e0a7cbd4eb2615d003b0f5e0c18f2659c
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-02-03 21:05:35 -0300

    update slide swiper js

--- cnsa-web/src/assets/components/Banner.jsx
+++ cnsa-web/src/assets/components/Banner.jsx
@@ -5,23 +5,40 @@ import banner2 from '../images/cnsa-banner-default-2.png';
 import banner3 from '../images/cnsa-banner-default-3.png';
 import banner4 from '../images/cnsa-banner-default-4.png';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const Banner = () => {
 
     return (
-        <section className={styles.sliderContainer}>
-                <div>
-                    <img src={banner1} alt="banner logo"/>
-                </div>
-                <div>
-                    <img src={banner2} alt="banner logo 2"/>
-                </div>
-                <div>
-                    <img src={banner3} alt="banner logo 3"/>
-                </div>
-                <div>
-                    <img src={banner4} alt="banner logo 4"/>
-                </div>
-        </section>
+        <>
+            <section className={styles.sliderContainer}>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    // navigation
+                    pagination={{ clickable: true }}
+                    // scrollbar={{ draggable: true }}
+                    loop={true}
+                    autoplay={{ 
+                        delay: 4000, 
+                        disableOnInteraction: false 
+                    }}
+                >
+                    <SwiperSlide>
+                        <img src={banner1} alt="banner logo" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner2} alt="banner logo 2" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner3} alt="banner logo 3" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner4} alt="banner logo 4" />
+                    </SwiperSlide>
+                </Swiper>
+            </section>
+        </>
     );
 };
 
