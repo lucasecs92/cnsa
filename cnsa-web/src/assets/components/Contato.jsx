1e144e36d5708a5ac8776b1281394bddd20c042c
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-02-07 11:00:47 -0300

    update

--- cnsa-web/src/assets/components/Contato.jsx
+++ cnsa-web/src/assets/components/Contato.jsx
@@ -8,13 +8,13 @@ const Contato = () => {
                 <section className={styles.contatoWrap}>
                     <div className={styles.contatoTopo}>
                         <h2>Fale Conosco</h2>
-                        <a href="">contato@escola.com.br</a>
+                        <a href="#">contato@escola.com.br</a>
                     </div>
                     <form className={styles.contatoForm}>
                         <input type="text" id='nome' name='name' placeholder='Nome' required className={styles.inputs}/>
                         <input type="email" id='email' name='email' placeholder='E-mail' required className={styles.inputs}/>
                         <textarea name="message" id="message" cols="30" rows="10" placeholder='Mensagem' required></textarea>
-                        <input type="submit" value="ENVIAR" className={styles.submitBtn}/>
+                        <input type="submit" value="Enviar" className={styles.submitBtn}/>
                     </form>
                 </section>
             </section>
