26e2873cbe0feb73059f039be6247230bd44da4b
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-04-03 14:25:00 -0300

    update

--- cnsa-web/src/assets/components/Banner/index.jsx
+++ cnsa-web/src/assets/components/Banner/index.jsx
@@ -4,6 +4,7 @@ import banner1 from '../../images/cnsa-banner-default.png';
 import banner2 from '../../images/cnsa-banner-default-2.png';
 import banner3 from '../../images/cnsa-banner-default-3.png';
 import banner4 from '../../images/cnsa-banner-default-4.png';
+import banner5 from '../../images/banner-volta-as-aulas.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -12,31 +13,34 @@ const Banner = () => {
     return (
         <>
             <section className="sliderContainer">
-                    <Swiper
-                        spaceBetween={0}
-                        slidesPerView={1}
-                        // navigation
-                        pagination={{ clickable: true }}
-                        // scrollbar={{ draggable: true }}
-                        loop={true}
-                        autoplay={{
-                             delay: 4000,
-                             disableOnInteraction: false
-                        }}
-                    >
-                        <SwiperSlide>
-                            <img src={banner1} alt="banner logo" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={banner2} alt="banner logo 2" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={banner3} alt="banner logo 3" />
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <img src={banner4} alt="banner logo 4" />
-                        </SwiperSlide>
-                    </Swiper>
+                <Swiper
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    // navigation
+                    pagination={{ clickable: true }}
+                    // scrollbar={{ draggable: true }}
+                    loop={true}
+                    autoplay={{
+                            delay: 4000,
+                            disableOnInteraction: false
+                    }}
+                >
+                    <SwiperSlide>
+                        <img src={banner1} alt="banner logo" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner2} alt="banner logo 2" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner3} alt="banner logo 3" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner4} alt="banner logo 4" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={banner5} alt="banner logo 5" />
+                    </SwiperSlide>
+                </Swiper>
             </section>
         </>
     );
