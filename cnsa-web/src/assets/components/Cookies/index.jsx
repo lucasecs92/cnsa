2003703f92a355871674df97d8992cb324629c0d
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-04-07 14:13:50 -0300

    update

--- cnsa-web/src/assets/components/Cookies/index.jsx
+++ cnsa-web/src/assets/components/Cookies/index.jsx
@@ -45,10 +45,12 @@ const Cookies = () => {
           <main>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem perspiciatis consequatur magni voluptates nam eligendi alias repellendus quasi minus, atque veniam totam sunt, est officia dignissimos? Dolor facere reprehenderit, vel molestias consectetur, deserunt dignissimos debitis ad doloremque eius assumenda non mollitia rem! Quae eius distinctio natus sapiente voluptas fugiat. Quidem tempora quibusdam provident, molestiae, magnam soluta pariatur necessitatibus quisquam et eligendi at fugit accusantium? Sit officia enim minus sapiente illo distinctio doloremque recusandae, voluptate quia culpa modi consequatur quas eos blanditiis corporis neque aut asperiores ex temporibus soluta in tenetur dolorem et repudiandae! Expedita corporis dicta ad magni corrupti magnam!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At dicta dolores asperiores, eaque nihil atque voluptatibus accusamus! Velit illum sint blanditiis quae harum modi, obcaecati voluptate rerum nihil! Id est, sapiente, quam accusantium atque placeat odio obcaecati commodi, aliquid unde dicta? Ad iusto adipisci repellat accusamus assumenda ipsam odit iste placeat aliquid hic quibusdam mollitia praesentium nihil dicta doloremque sunt blanditiis tempora cum nobis ducimus dolorem, ipsa, distinctio sequi a! Excepturi nihil quia dolorum quos, corrupti est! Quod unde blanditiis numquam maiores ducimus voluptatem maxime fuga sint cumque voluptatibus officia dolore, inventore error ab! Eum velit perspiciatis reiciendis, incidunt aliquam magni dicta, laudantium aut commodi aliquid, totam quas voluptate dignissimos! Asperiores distinctio assumenda autem! Cum consequatur autem dolorum obcaecati asperiores consectetur porro veritatis ab perferendis possimus? Quis dicta veniam quos corporis! Sapiente nesciunt ullam animi dolor voluptates nulla illum aspernatur perferendis obcaecati illo? Culpa atque deserunt officiis. Dolore ipsum culpa tempore. 
             </p>
+            
           </main>
           <footer>
-            <button onClick={handleCloseDialog}>OK</button>
+            <button aria-label="Fechar diálogo" onClick={handleCloseDialog}>OK</button>
           </footer>
         </dialog>
       </section>}
